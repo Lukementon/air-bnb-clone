6418bb965f0f09c873b817ed1f268357b7cb2f14
Luke Menton <lukementon15@gmail.com>
2021-08-07 21:24:48 +0100

    style change

--- components/Header.js
+++ components/Header.js
@@ -11,12 +11,13 @@ const Header = () => {
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left */}
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+      <div className="relative flex items-center h-10  my-auto">
         <Image
           src="https://links.papareact.com/qd3"
           layout="fill"
           objectFit="contain"
           objectPosition="left"
+          className="cursor-pointer"
         />
       </div>
 
