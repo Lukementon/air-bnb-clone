f2c92f6bb043fdf2f36cf4094f6569f8769097fb
Luke Menton <lukementon15@gmail.com>
2021-08-09 12:15:07 +0100

    day4 build

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoibHVrZW1lbnRvbiIsImEiOiJja3M0Z3IwMnkybnczMnVxajVneDh6ZHZ3In0.muXauBNTXIhFo1trdWIoeg",
+  },
 };
--- pages/search.js
+++ pages/search.js
@@ -3,6 +3,7 @@ import Footer from "../components/Footer";
 import { useRouter } from "next/dist/client/router";
 import { format } from "date-fns";
 import InfoCard from "../components/InfoCard";
+import Map from "../components/Map";
 
 const Search = ({ searchResults }) => {
   const router = useRouter();
@@ -52,6 +53,10 @@ const Search = ({ searchResults }) => {
               )
             )}
           </div>
+        </section>
+
+        <section className="hidden xl:inline-flex min-w-[600px]">
+          <Map searchResults={searchResults} />
         </section>
       </main>
 
